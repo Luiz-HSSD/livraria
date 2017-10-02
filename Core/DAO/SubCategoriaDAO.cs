d0a24099af0be99c3144cc55c84a1c95956464f1
Luiz Henrique Santos Sousa Diniz <lhenrique_diniz@hotmail.com>
2017-10-02 06:36:48 -0300

    implantando modelo

---
+++ Core/DAO/SubCategoriaDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Core.DAO
+{
+    public class SubCategoriaDAO : AbstractDAO
+    {
+        public SubCategoriaDAO() : base("", "")
+        {
+        }
+
+        public override void Alterar(EntidadeDominio entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<EntidadeDominio> Consultar(EntidadeDominio entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Salvar(EntidadeDominio entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Negocio/Persistir_livro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using Core.DAO;
+
+namespace Core.Negocio
+{
+    class Persistir_livro : Abstract_Regra_de_Negocios
+    {
+        public override string Processar(EntidadeDominio entidade)
+        {
+            AutorDAO Adao = new AutorDAO();
+
+            LivroDAO lDAO = new LivroDAO();
+            throw new NotImplementedException();
+        }
+    }
+}
